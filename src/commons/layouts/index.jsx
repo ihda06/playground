481aa21e507d332e0bdf25c02b13f0b49861be9c
ihda06 <ihdaanwari5@gmail.com>
2023-09-11 11:32:23 +0700

    refactor: filenaming

--- src/commons/layouts/index.jsx
+++ src/commons/layouts/index.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
-import { Sidebar } from "./sidebar/Index";
+import { Sidebar } from "./sidebar";
+
 
 export const Layouts = ({ children }) => {
   return (
@@ -7,7 +8,7 @@ export const Layouts = ({ children }) => {
       <div className="flex flex-col justify-center lg:flex-row lg:gap-5 lg:pt-10">
         <div className="flex flex-col lg:flex-row w-full justify-center lg:gap-5">
           <header>
-            <Sidebar />
+            <Sidebar></Sidebar>
           </header>
 
           <main className="lg:max-w-[854px] transition-all duration-300 w-full lg:h-screen overflow-y-auto no-scrollbar">
